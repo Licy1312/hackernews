c7a4a9c24b0d99c637fba535e00a4dae06f15d64
yage <13156525583@163.com>
2016-06-28 21:46:13 +0800

    The comments and notifications have been completed.

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -1,3 +1,5 @@
+
+
 Template.postSubmit.events({
   'submit form': function(e) {
     e.preventDefault();
@@ -10,10 +12,10 @@ Template.postSubmit.events({
     Meteor.call('postInsert', post, function(error, result) {
       // 显示错误信息并退出
       if (error)
-        return alert(error.reason);
+        return throwError(error.reason);
      // 显示结果，跳转页面
       if (result.postExists)
-        alert('This link has already been posted（该链接已经存在）');
+        throwError('This link has already been posted（该链接已经存在）');
      
       Router.go('postPage', {_id: result._id});
     });
--- server/publications.js
+++ server/publications.js
@@ -1,3 +1,12 @@
 Meteor.publish('allPosts',function(){
 	return Posts.find();
 });
+
+Meteor.publish('comments', function(postId) {
+	check(postId,String);
+    return Comments.find({postId:postId});
+});
+
+Meteor.publish('notifications', function() {
+  return Notifications.find({userId: this.userId, read: false});
+});
