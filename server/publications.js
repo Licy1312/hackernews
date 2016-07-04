d0efd9b446a9f6005f7b35977ef9e37dd66d0321
yage <13156525583@163.com>
2016-07-04 22:01:55 +0800

    paging

--- server/publications.js
+++ server/publications.js
@@ -1,5 +1,9 @@
-Meteor.publish('allPosts',function(){
-	return Posts.find();
+Meteor.publish('posts',function(options){
+	check(options,{
+		sort:Object,
+		limit:Number
+	});
+	return Posts.find({},options);
 });
 
 Meteor.publish('comments', function(postId) {
